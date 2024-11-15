a634a2a7a9c9f444ab9280d51806ec2bf2f6adb9
Angelita Medina <aam246812@gmail.com>
2024-11-15 14:04:02 -0600

    started home page
    started the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -4,5 +4,28 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+       <h3>Upcoming Events</h3>
+        <div className = {styles.UEvents}>
+        
+          <div className = {styles.eventBox}>
+            <h>Event 1</h>
+            <p>When</p>
+            <p>Where</p>
+            <p>time</p>
+          </div>
+          <div className =  {styles.eventBox}>
+            <h>Event 2</h>
+            <p>When</p>
+            <p>Where</p>
+            <p>time</p>
+          </div>
+          <div className =  {styles.eventBox1}>
+            <h>Event 3</h>
+            <p>When</p>
+            <p>Where</p>
+            <p>time</p>
+          </div>
+        </div>
     </div>
   ) }
+
